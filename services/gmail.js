ed77eb74d9845c09ccfcbd491ae9952460f7bfdf
Lucas Cardozo <cardozoconc@gmail.com>
2025-04-24 18:00:34 -0300

    Create gmail.js

---
+++ services/gmail.js
@@ -0,0 +1,72 @@
+
+
+const { google } = require('googleapis');
+const { getOAuthClient } = require('./googleAuth');
+const base64url = require('base64url');
+
+// Send an email
+async function sendEmail({ to, subject, message }) {
+  const auth = getOAuthClient();
+  const gmail = google.gmail({ version: 'v1', auth });
+
+  const emailLines = [
+    `To: ${to}`,
+    'Content-Type: text/plain; charset=utf-8',
+    'MIME-Version: 1.0',
+    `Subject: ${subject}`,
+    '',
+    message
+  ];
+
+  const email = emailLines.join('\n');
+  const encodedMessage = base64url.encode(email);
+
+  const res = await gmail.users.messages.send({
+    userId: 'me',
+    requestBody: {
+      raw: encodedMessage
+    }
+  });
+
+  return res.data;
+}
+
+// List recent emails
+async function listEmails(maxResults = 5) {
+  const auth = getOAuthClient();
+  const gmail = google.gmail({ version: 'v1', auth });
+
+  const res = await gmail.users.messages.list({
+    userId: 'me',
+    maxResults
+  });
+
+  const messages = res.data.messages || [];
+  const messageDetails = [];
+
+  for (const msg of messages) {
+    const fullMessage = await gmail.users.messages.get({
+      userId: 'me',
+      id: msg.id
+    });
+
+    const subjectHeader = fullMessage.data.payload.headers.find(
+      h => h.name === 'Subject'
+    );
+
+    const snippet = fullMessage.data.snippet;
+
+    messageDetails.push({
+      id: msg.id,
+      subject: subjectHeader ? subjectHeader.value : '(No subject)',
+      snippet
+    });
+  }
+
+  return messageDetails;
+}
+
+module.exports = {
+  sendEmail,
+  listEmails
+};
